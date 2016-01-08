9263f1ef807ba80e06ca1f36af5a401a50e1fe76
gabriel.munteanu <gabriel.munteanu@gmail.com>
2016-01-08 11:42:50 +0200

    lots of changes Ghost Run Different cameras Some rewrites 2

---
+++ app/src/main/java/ro/redmotor/kartgame/Drawables/CamControlDO.java
@@ -0,0 +1,80 @@
+package ro.redmotor.kartgame.Drawables;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+
+import ro.redmotor.kartgame.IGameControl;
+import ro.redmotor.kartgame.R;
+
+/**
+ * Created by Gabi on 1/5/2016.
+ */
+public class CamControlDO extends DrawableObject implements IGameControl {
+
+    private Bitmap cam;
+
+    //cam detection bounds
+    private float boundX1b;
+    private float boundX2b;
+    private float boundY1b;
+    private float boundY2b;
+
+    private double selection = 0;
+
+
+    public CamControlDO(Scene scene, boolean debug) {
+        super(scene, debug);
+
+        scene.setCam(this);
+
+        boundX1b = scene.metersWidth(0.2f);
+        boundX2b = scene.metersWidth(1.2f);
+        boundY1b = scene.metersHeight(scene.getHeightInM() - 5f);
+        boundY2b = scene.metersHeight(scene.getHeightInM() - 4f);
+
+    }
+
+    @Override
+    public void loadObject(Context context) {
+        cam = BitmapFactory.decodeResource(context.getResources(), R.drawable.cam);
+
+    }
+
+    @Override
+    protected void drawObject(Canvas canvas) {
+        canvas.translate(boundX1b + (boundX2b - boundX1b) / 2, boundY1b + (boundY2b - boundY1b) / 2);
+        float scaleX = (canvas.getWidth()/ scene.getWidthInM()) / (cam.getWidth()/ 1f);
+        canvas.scale(scaleX, scaleX);
+
+        canvas.drawBitmap(cam, -cam.getWidth() / 2, -cam.getHeight() / 2, null);
+
+    }
+
+    @Override
+    public void checkPointerDown(int pointerId, float x, float y) {
+        if (x < boundX2b && x > boundX1b && y < boundY2b && y > boundY1b) {
+            this.selection = 1;
+        }
+    }
+
+    @Override
+    public void checkPointerUp(int pointerId, float x, float y) {
+
+    }
+
+    @Override
+    public void checkPointerMove(int pointerId, float x, float y) {
+
+    }
+
+    @Override
+    public double getSelection() {
+        if (selection == 1) {
+            selection = 0;
+            return 1;
+        }
+        return 0;
+    }
+}
---
+++ app/src/main/java/ro/redmotor/kartgame/Drawables/Scene.java
@@ -0,0 +1,219 @@
+package ro.redmotor.kartgame.Drawables;
+
+import android.content.Context;
+import android.graphics.Canvas;
+
+import java.util.ArrayList;
+
+import ro.redmotor.kartgame.Cameras.FixedKartCamera;
+import ro.redmotor.kartgame.Cameras.ICamera;
+import ro.redmotor.kartgame.Game.Engine.Game;
+import ro.redmotor.kartgame.Game.Utilities.Point;
+
+/**
+ * Created by Gabi on 1/4/2016.
+ */
+public class Scene {
+
+    private Game game;
+    private DrawableObject track;
+    private DrawableObject kart;
+    private DrawableObject steeringWheel;
+    private DrawableObject pedals;
+    private DrawableObject telemetry;
+    private DrawableObject ghost;
+    private DrawableObject cam;
+
+    private ICamera currentCamera;
+    private ArrayList<ICamera> cameras;
+
+    protected Point screenSize;
+    private Float aspectRatio;
+    private Float pixelsPerMeterWidth;
+    private Float pixelsPerMeterHeight;
+    protected boolean debug;
+    private final int METERS_PER_SCREEN_WIDTH = 10;
+
+    private Point vehiclePosition;
+    private double vehicleAngle;
+
+    public Scene(Point screenSize, Game game) {
+        this.screenSize = screenSize;
+        this.game = game;
+    }
+
+
+    public Game getGame() {
+        return game;
+    }
+
+    public DrawableObject getTrack() {
+        return track;
+    }
+
+    public void setTrack(DrawableObject track) {
+        this.track = track;
+    }
+
+    public void setKart(DrawableObject kart) {
+        this.kart = kart;
+    }
+
+    public void setSteeringWheel(DrawableObject steeringWheel) {
+        this.steeringWheel = steeringWheel;
+    }
+
+    public void setPedals(DrawableObject pedals) {
+        this.pedals = pedals;
+    }
+
+    public void setTelemetry(DrawableObject telemetry) {
+        this.telemetry = telemetry;
+    }
+
+    public void setGhost(DrawableObject ghost) {
+        this.ghost = ghost;
+    }
+
+    public void setCam(DrawableObject cam) {
+        this.cam = cam;
+    }
+
+    public Point getVehiclePosition() {
+        return vehiclePosition;
+    }
+
+    public double getVehicleAngle() {
+        return vehicleAngle;
+    }
+
+    public void setCameras(ArrayList<ICamera> cameras) {
+        this.cameras = cameras;
+        //call this so we have a cam set
+        nextCam();
+
+    }
+
+    public void loadDrawables(Context context) {
+        //load track
+        track.loadObject(context);
+
+        //load kart
+        kart.loadObject(context);
+        ghost.loadObject(context);
+
+        //load steering control
+        steeringWheel.loadObject(context);
+        //load throttle braking control
+        pedals.loadObject(context);
+
+        //load camera
+        cam.loadObject(context);
+
+    }
+
+    public void drawScene(Canvas canvas) {
+
+        //first pickup current vehicle position and angle
+        //so we don't multiple read this
+        vehiclePosition = new Point(game.getVehicle().getPosition().getX(),game.getVehicle().getPosition().getY());
+        vehicleAngle = game.getVehicle().getAngle();
+
+        int initialCanvas = canvas.save();
+
+        //scale track to fit devices, so we have the exact same meter width on all
+        float trackScale = (float)game.getTrack().getScale();
+        float scaleFactor = getPixelsPerMeterWidth() / trackScale;
+        canvas.scale(scaleFactor,scaleFactor);
+
+        //apply the currently selected camera
+        if (currentCamera!=null) {
+            currentCamera.applyCamera(canvas);
+        }
+
+        //draw scene
+        track.draw(canvas);
+        ghost.draw(canvas);
+        kart.draw(canvas);
+
+        //restore canvas for overlays
+        canvas.restoreToCount(initialCanvas);
+        //draw controls
+        steeringWheel.draw(canvas);
+        pedals.draw(canvas);
+
+
+        //draw telemetry
+        telemetry.draw(canvas);
+
+        //draw camera control
+        cam.drawObject(canvas);
+    }
+
+
+    public void nextCam() {
+        if (cameras==null || cameras.size() == 0) return;
+        if (currentCamera == null) {
+            currentCamera = cameras.get(0);
+        } else {
+            int index = cameras.indexOf(currentCamera);
+            //if by any chance index is -1 then just return cameras[0]
+            index++;
+            if (index<cameras.size()) {
+                currentCamera = cameras.get(index);
+            }
+            else currentCamera = cameras.get(0);
+        }
+
+    }
+
+
+    // HELPER METHODS beyond this point
+
+    public float getAspectRatio() {
+        if (aspectRatio == null)  aspectRatio = (float)(screenSize.getY() / screenSize.getX());
+        return aspectRatio;
+    }
+
+
+    public Point getScreenSize() {
+        return this.screenSize;
+    }
+
+    public float getWidthInM() {
+        return METERS_PER_SCREEN_WIDTH;
+    }
+
+    public float getHeightInM() {
+        return METERS_PER_SCREEN_WIDTH * getAspectRatio();
+    }
+
+    public float getPixelsPerMeterWidth() {
+        if (pixelsPerMeterWidth == null) pixelsPerMeterWidth = (float)(screenSize.getX() / getWidthInM());
+        return pixelsPerMeterWidth;
+    }
+
+    public float getPixelsPerMeterHeight() {
+        if (pixelsPerMeterHeight == null) pixelsPerMeterHeight = (float)(screenSize.getY() / getHeightInM());
+        return pixelsPerMeterHeight;
+    }
+
+    /**
+     * Helper method to translate meters in usable pixels
+     * @param meters
+     * @return
+     */
+    protected float metersHeight(float meters) {
+        return meters * getPixelsPerMeterHeight();
+    }
+
+    /**
+     * Helper method to translate meters in usable pixels
+     * @param meters
+     * @return
+     */
+    protected float metersWidth(float meters) {
+        return meters * getPixelsPerMeterWidth();
+    }
+
+}
